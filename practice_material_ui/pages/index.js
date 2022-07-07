edb46180c0593aa1f8f8f3489710e80cb20b5f99
haagmin5382 <itworker5382@gmail.com>
2022-07-07 21:25:51 +0900

    feat : add material Tab

--- practice_material_ui/pages/index.js
+++ practice_material_ui/pages/index.js
@@ -1,9 +1,22 @@
 import MaterialButton from "../components/MaterialButton";
 import Title from "../components/Title";
-
+import { AppBar, Tabs, Tab } from "@material-ui/core";
+import { useState } from "react";
 export default function Home() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="Main Tabs">
+          <Tab label="Home" />
+          <Tab label="Send" />
+          <Tab label="Profile" />
+        </Tabs>
+      </AppBar>
       <Title />
       <MaterialButton />
     </>
